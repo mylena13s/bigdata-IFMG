a3c13978428169990c5df2f7d1122dc4208b740c
Mylena Santos <2214212@aluno.univesp.br>
2024-04-04 20:06:55 -0300

    add: io

---
+++ Input and print/io.py
@@ -0,0 +1,9 @@
+print('Bem vindo!')
+# Nome do usuário 
+nome = input('Informe seu nome: ')
+# Idade convertida para inteiro
+idade = int(input('Informe sua idade: '))
+print() # Escreve uma linha em branco
+# Escreve uma mensagem usando as variáveis
+print('Olá,', nome, 'sua idade é', idade)
+print('Até mais')
