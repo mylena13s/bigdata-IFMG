0d2a6b4a0daf360a7db8456afbca476db3b7eae9
Mylena Santos <2214212@aluno.univesp.br>
2024-04-04 20:56:54 -0300

    add: nested and consecutive decision structure example

---
+++ Nested and consecutive decision structure/nes_conse.py
@@ -0,0 +1,22 @@
+#ESTRUTURAS DE DECISÃO ANINHADAS
+
+n = float(input('Nota: '))
+if n >= 60:
+    print('Aprovado')
+else:
+    if n >= 40:
+        print('Reavaliação')
+    else:    
+        print('Reprovado') 
+print ('Boas férias')  
+
+#ESTRUTURAS DE DECISÃO CONSECUTIVAS
+
+n = float(input('Nota: '))
+if n >= 60:
+    print('Aprovado')
+elif n >= 40:
+        print('Reavaliação')
+else:    
+        print('Reprovado') 
+print ('Boas férias')  
