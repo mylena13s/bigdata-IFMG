6fab31764ca47de018d9bcc94006af600b2d4eea
Mylena Santos <2214212@aluno.univesp.br>
2024-04-04 20:49:52 -0300

    add: simple and compound decision

---
+++ Decision structure/dec_structure.py
@@ -0,0 +1,15 @@
+# ESTRUTURA DE DECISÃO SIMPLES
+
+nota = float(input('Nota: '))
+if nota >= 60:
+    print('Aprovado')
+print ('Boas férias')
+
+# ESTRUTURA DE DECISÃO COMPOSTA
+
+n = float(input('Nota: '))
+if n >= 60:
+    print('Aprovado')
+else:
+    print('Reprovado') 
+print ('Boas férias')      
