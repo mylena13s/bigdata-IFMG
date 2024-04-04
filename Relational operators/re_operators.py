b9be95c31ee69f3958060bc2faaae216fc7a54c4
Mylena Santos <2214212@aluno.univesp.br>
2024-04-04 20:25:02 -0300

    add: relational operators

---
+++ Relational operators/re_operators.py
@@ -0,0 +1,10 @@
+# EXEMPLOS
+
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe outro número: '))
+print(n1, '==', n2, '->', n1 == n2)
+print(n1, '!=', n2, '->', n1 != n2)
+print(n1, '<-', n2, '->', n1 < n2)
+print(n1, '<=', n2, '->', n1 <= n2)
+print(n1, '>', n2, '->', n1 > n2)
+print(n1, '>=', n2, '->', n1 >= n2)
