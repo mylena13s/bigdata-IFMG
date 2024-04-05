928177962e330021dd4604ef0c3b2c2bb845318e
Mylena Santos <2214212@aluno.univesp.br>
2024-04-04 21:13:02 -0300

    add: listing of numbers and sum of 10 numbers

---
+++ Looping/loop_examples.py
@@ -0,0 +1,15 @@
+# LISTAGEM DE NÚMEROS PARES (WHILE)
+
+atual = 0
+n = int(input('Informe um número: '))
+while atual < n:
+    print(atual)
+    atual += 2
+
+# SOMA DE 10 NÚMEROS (FOR)    
+    
+soma = 0 
+for count in range(10):
+    n = float(input('Informe um número: '))    
+    soma += n 
+print(soma)    
